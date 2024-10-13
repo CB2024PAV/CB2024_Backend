d8bf0ed82e79c6c8e077d156680f55dab666a5f6
Chuckoo-l2l <vivek279.venky@gmail.com>
2024-10-12 19:10:26 -0700

    added cors

--- API/endpoints.py
+++ API/endpoints.py
@@ -1,8 +1,12 @@
 from flask import Flask,request
 from login_page import login_api
 from qdrant import qdrant_api
+from flask_cors import CORS
+
+
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(login_api)
 app.register_blueprint(qdrant_api)
 
