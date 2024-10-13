6f2f65de985d8ec74deab6dbe7f3a02458c34c0a
Chuckoo-l2l <vivek279.venky@gmail.com>
2024-10-13 02:02:35 -0700

    search by image implemented

--- API/twelve_labs.py
+++ API/twelve_labs.py
@@ -10,6 +10,17 @@ twelve_api_key = os.environ.get("TWELVE_API_KEY")
 client = TwelveLabs(api_key=twelve_api_key)
 index = os.environ.get("TWELVE_INDEX")
 
+def get_video_url(video_id):
+    url = "https://api.twelvelabs.io/v1.2/indexes/{}/videos/{}".format(index,video_id)
+    print(url)
+    headers = {
+        "accept": "application/json",
+        "Content-Type": "application/json",
+        "x-api-key":twelve_api_key
+    }
+    response = requests.get(url, headers=headers)
+    return response.json()["hls"]["video_url"]
+
 @twelvelabs_api.route("/search_by_text", methods=['GET'])
 def search_by_text():
     query = request.args.get('query')
@@ -21,18 +32,35 @@ def search_by_text():
 
     try:
         video_id = search_results.data[0].video_id
+        if video_id:
+            return {"data":get_video_url(video_id),"message":{},"success":True}
     except:
         return {"data":{},"message":{"Error 763"},"success":False}
-
-    if video_id:
-        url = "https://api.twelvelabs.io/v1.2/indexes/{}/videos/{}".format(index,video_id)
-        print(url)
-        headers = {
-            "accept": "application/json",
-            "Content-Type": "application/json",
-            "x-api-key":twelve_api_key
-        }
-        response = requests.get(url, headers=headers)
-        return {"data":response.json()["hls"]["video_url"],"message":{},"success":True}
     
     return {"data":{},"message":{"Error 764"},"success":False}
+
+@twelvelabs_api.route("/search_by_image", methods=['POST'])
+def search_by_image():
+    if 'image' not in request.files:
+        return {"data":{},"message":{"Error 765"},"success":False}
+    
+    image = request.files['image']
+    image.save('./image.jpg')
+
+    search_results = client.search.query(
+        index_id=index,
+        query_media_type="image",
+        query_media_file="./image.jpg", 
+        options=["visual"]
+    )
+
+    try:
+        video_id = search_results.data[0].video_id
+        if video_id:
+            return {"data":get_video_url(video_id),"message":{},"success":True}
+    except:
+        return {"data":{},"message":{"Error 766"},"success":False}
+    finally:
+        os.remove("./image.jpg")
+    
+    return {"data":{},"message":{"Error 767"},"success":False}
